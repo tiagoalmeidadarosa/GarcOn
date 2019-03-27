4629828a8c0becfc09aef7ec2bd576877da7b4d3
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2019-03-27 00:20:29 -0300

    BasketPage Parcial

--- GarcOn/GarcOn/App.xaml.cs
+++ GarcOn/GarcOn/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GarcOn.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace GarcOn
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new BasketPage();
         }
 
         protected override void OnStart()
---
+++ GarcOn/GarcOn/Pages/BasketPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GarcOn.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BasketPage : ContentPage
+    {
+        public BasketPage()
+        {
+            InitializeComponent();
+
+            List<OrderItem> items = new List<OrderItem>();
+            items.Add(new OrderItem("Hamburguer", "A hamburguer, beefburguer or burger is a sandwich consisting of one or more coocked patties of ground...", "100"));
+
+            orders.ItemsSource = items;
+        }
+
+        public class OrderItem
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string TotalPrice { get; set; }            
+
+            public OrderItem(string name, string description = "", string totalPrice = "")
+            {
+                this.Name = name;
+                this.Description = description;
+                this.TotalPrice = totalPrice;
+            }
+
+            public override string ToString()
+            {
+                return this.Name;
+            }
+        }
+    }
+}
