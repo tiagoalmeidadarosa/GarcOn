9380d41c24f2f13d8f70500673ae75278d7ee609
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2019-04-21 22:32:40 -0300

    ActivityIndicator ao buscar atualizações de produtos

--- GarcOn/GarcOn/Pages/ConfigurationPage.xaml.cs
+++ GarcOn/GarcOn/Pages/ConfigurationPage.xaml.cs
@@ -5,6 +5,8 @@ using Newtonsoft.Json;
 using Rg.Plugins.Popup.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,11 +14,14 @@ using Xamarin.Forms.Xaml;
 namespace GarcOn.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ConfigurationPage : ContentPage
+    public partial class ConfigurationPage : ContentPage, INotifyPropertyChanged
     {
         public ConfigurationPage()
         {
             InitializeComponent();
+
+            IsLoading = false;
+            BindingContext = this;
 
             MessagingCenter.Subscribe<PasswordPopupPage>(this, "Refresh", (s) => {
                 RefreshProperties();
@@ -53,22 +58,35 @@ namespace GarcOn.Pages
 
         private async void OnSaveAndGetUpdate(object sender, EventArgs e)
         {
-            DependencyService.Get<IProgressDialog>().LoadingShow();
+            ShowActivityIndicator();
+
+            await Task.Delay(2000);
 
             if (!string.IsNullOrEmpty(txtIP.Text) && !string.IsNullOrEmpty(txtNumeroMesa.Text))
             {
-                await SecureStorage.SetAsync("ip_servidor", txtIP.Text);
-                await SecureStorage.SetAsync("numero_mesa", txtNumeroMesa.Text);
-
-                //Buscar atualizações
-                APIService apiService = new APIService(txtIP.Text);
-                var jsonCategoriesAndProducts = await apiService.GetCategoriesAndProducts();
-
-                if (!string.IsNullOrEmpty(jsonCategoriesAndProducts))
+                try
                 {
-                    App.Categorias = JsonConvert.DeserializeObject<List<Categoria>>(jsonCategoriesAndProducts);
+                    await SecureStorage.SetAsync("ip_servidor", txtIP.Text);
+                    await SecureStorage.SetAsync("numero_mesa", txtNumeroMesa.Text);
 
-                    await SecureStorage.SetAsync("categorias_e_produtos", jsonCategoriesAndProducts);
+                    //Buscar atualizações
+                    APIService apiService = new APIService(txtIP.Text);
+                    var jsonCategoriesAndProducts = await apiService.GetCategoriesAndProducts();
+
+                    if (!string.IsNullOrEmpty(jsonCategoriesAndProducts))
+                    {
+                        App.Categorias = JsonConvert.DeserializeObject<List<Categoria>>(jsonCategoriesAndProducts);
+                        await SecureStorage.SetAsync("categorias_e_produtos", jsonCategoriesAndProducts);
+
+                        await DisplayAlert("Atualização concluída", "Informações carregadas com sucesso!", "Cancelar");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex != null)
+                    {
+                        await DisplayAlert("Não foi possível obter as informações", "Ocorreu um erro: " + ex.Message, "Cancelar");
+                    }
                 }
             }
             else
@@ -76,7 +94,40 @@ namespace GarcOn.Pages
                 await DisplayAlert("Não foi possível salvar", "É necessário o preenchimento de todos os campos.", "Cancelar");
             }
 
-            DependencyService.Get<IProgressDialog>().LoadingHide();
+            HideActivityIndicator();
+        }
+
+        public async void ShowActivityIndicator()
+        {
+            IsLoading = true;
+        }
+
+        public async void HideActivityIndicator()
+        {
+            IsLoading = false;
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return this.isLoading;
+            }
+            set
+            {
+                this.isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
