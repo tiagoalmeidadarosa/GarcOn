d33490ed6a1b27eb58a733c588289bfddd591a02
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2019-04-25 22:51:25 -0300

    Alteração para deixar a FoodDetailPage dinâmica e mudanças nos estilos de fontes

--- GarcOn/GarcOn/Pages/FoodDetailPage.xaml.cs
+++ GarcOn/GarcOn/Pages/FoodDetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GarcOn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,71 @@ namespace GarcOn.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class FoodDetailPage : ContentPage
 	{
-		public FoodDetailPage ()
+        const int MinValue = 1;
+        const int MaxValue = 50;
+
+		public FoodDetailPage(Produto produto)
 		{
-			InitializeComponent ();
-		}
-	}
+			InitializeComponent();
+
+            lblTitle.Text = produto.Nome;
+            lblPrice.Text = "R$ " + produto.Valor.ToString("N2");
+            lblDescription.Text = produto.Descricao;
+
+            lblQtd.Text = MinValue.ToString();
+
+            CalculateTotalPrice(produto.Valor, MinValue);
+        }
+
+        private async void PlusButton_OnClicked(object sender, EventArgs e)
+        {
+            var quantidade = Convert.ToInt16(lblQtd.Text);
+            quantidade++;
+
+            if (quantidade <= MaxValue)
+            {
+                try
+                {
+                    await lblQtd.ScaleTo(1.3, 100);
+                    lblQtd.Text = quantidade.ToString();
+                    await lblQtd.ScaleTo(1, 100);
+
+                    var valor = Convert.ToDouble(lblPrice.Text.Replace("R$ ", ""));
+                    CalculateTotalPrice(valor, quantidade);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+
+        private async void MinusButton_OnClicked(object sender, EventArgs e)
+        {
+            var quantidade = Convert.ToInt16(lblQtd.Text);
+            quantidade--;
+
+            if (quantidade >= MinValue)
+            {
+                try
+                {
+                    await lblQtd.ScaleTo(1.3, 100);
+                    lblQtd.Text = quantidade.ToString();
+                    await lblQtd.ScaleTo(1, 100);
+
+                    var valor = Convert.ToDouble(lblPrice.Text.Replace("R$ ", ""));
+                    CalculateTotalPrice(valor, quantidade);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+        }
+
+        private void CalculateTotalPrice(double value, int quantity)
+        {
+            lblTotalPrice.Text = "Preço total: R$ " + (value * quantity).ToString("N2");
+        }
+    }
 }
--- GarcOn/GarcOn/Views/FoodsListView.xaml.cs
+++ GarcOn/GarcOn/Views/FoodsListView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GarcOn.Models;
+using GarcOn.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,25 @@ namespace GarcOn.Views
 		{
 			InitializeComponent();
 		}
-	}
+
+        private async void FoodsList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return; //ItemSelected is called on deselection, which results in SelectedItem being set to null
+            }
+
+            var item = (Produto)e.SelectedItem;
+
+            if (item != null)
+            {
+                var foodDetailPage = new FoodDetailPage(item);
+                foodDetailPage.Title = item.Nome;
+
+                await Navigation.PushAsync(foodDetailPage);
+
+                FoodsList.SelectedItem = null;
+            }
+        }
+    }
 }
