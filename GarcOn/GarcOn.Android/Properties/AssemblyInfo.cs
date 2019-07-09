b96376d5be3e0fd4e3e2847512268928e1ea7100
Tiago Almeida da Rosa <tiago_almeida_rosa@hotmail.com>
2019-07-08 23:03:50 -0300

    Alteração no nome do aplicativo (GarcOn.Android para GarçOn)

--- GarcOn/GarcOn.Android/Properties/AssemblyInfo.cs
+++ GarcOn/GarcOn.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GarcOn.Android")]
+[assembly: AssemblyTitle("GarçOn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GarcOn.Android")]
+[assembly: AssemblyProduct("GarçOn")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
